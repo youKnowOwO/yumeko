19f6a9a0d4cefe5eda1893c8cc3266239d4a6744
youKnowOwO <meiterumidikolam@gmail.com>
2020-07-22 08:25:33 +0800

    https://emilia-api.xyz/api/sepia

---
+++ src/commands/Fun/ImageManipulation/Sepia.ts
@@ -0,0 +1,38 @@
+import type YumekoClient from "../../../classes/Client";
+import Command from "../../../classes/Command";
+import request from "node-superfetch";
+import type { Message } from "discord.js";
+
+export default class SepiaCommand extends Command {
+    public constructor (client: YumekoClient) {
+        super(client, "sepia", {
+            aliases: ["sepia"],
+            description: {
+                content: "Draws an image with sepia effect",
+                usage: "sepia [user|image]",
+                examples: ["sepia"]
+            },
+            category: "fun",
+            permissions: {
+                client: ["ATTACH_FILES"]
+            },
+            args: [
+                {
+                    identifier: "image",
+                    match: "rest",
+                    type: "image",
+                    default: (msg: Message): string => msg.author.displayAvatarURL({ format: "png", size: 512, dynamic: true })
+                }
+            ]
+        });
+    }
+
+    public async exec(msg: Message, { image } : { image: string }): Promise<Message> {
+        const m = await msg.channel.send("🖌️ **| Painting...**");
+        const { raw: attachment } = await request.get("https://emilia-api.xyz/api/sepia")
+            .set("Authorization", `Bearer ${process.env.EMIAPI}`)
+            .query({ image });
+        m.delete();
+        return msg.ctx.send({files:[{attachment, name: "sepia.png"}]});
+    }
+}
