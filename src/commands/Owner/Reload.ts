756c42fbadb79c364b6e13eaede61cc2eaae9409
OwO <41247578+youKnowOwO@users.noreply.github.com>
2020-07-27 23:41:46 +0700

    Nb

--- src/commands/Owner/Reload.ts
+++ src/commands/Owner/Reload.ts
@@ -18,7 +18,7 @@ export default class ReloadCommand extends Command {
             args: [
                 {
                     identifier: "dontBuild",
-                    type: "flag",
+                    match: "flag",
                     flag: "dbuild"
                 },
                 {
