be310ea8c0f5f0b2301d9d9d8e68a1e0332d7f77
OwO <41247578+youKnowOwO@users.noreply.github.com>
2020-08-19 03:52:57 +0000

    chore(game): Tic Tac Toe  🔢 (#45)
    * feat: Untested TicTacToe class
    
    * chore(TicTacToe): Fixing bugable statement and change param
    
    * feat(TicTacToe): Add AI like minimax concept
    
    * refactor(ttt): The Opponent args is now optional!

---
+++ src/classes/Games/TicTacToe.ts
@@ -0,0 +1,130 @@
+import { chunk } from "../../util/Util";
+
+interface TicTacToeToStringPayload {
+    sign: [string, string];
+    winSign: [string, string];
+    numbers: [string, string, string, string, string, string, string, string, string];
+    separator: string;
+}
+
+export default class TicTacToe {
+    public board = chunk(new Array(9).fill(0).map((_, i) => String(i + 1)), 3);
+    public end = false;
+    public winner?: string;
+    public solutions: [number, number][][] = [];
+    public turn = true; // true: player 1, false: player 2
+    public moves: [number, number][] = [];
+
+    public isEnd(): boolean {
+        return this.end;
+    }
+
+    public place(x: number, y: number): void {
+        this.board[x][y] = this.turn ? "X" : "O";
+        this.moves.push([x, y]);
+        if (this.isWin()) {
+            this.end = true;
+            this.winner = this.turn ? "X" : "O";
+        }
+        if (this.moves.length > 8) this.end = true;
+        if (!this.isEnd()) this.turn = !this.turn;
+    }
+
+    public canPlace(x: number, y: number): boolean {
+        if (!this.board[x] || !this.board[x][y]) return false;
+        return !["X", "O"].includes(this.board[x][y]);
+    }
+
+    public parsePosition(position: number): [number, number] {
+        position--;
+        return [
+            Math.floor(position/3),
+            position%3
+        ];
+    }
+
+    public isWin(): boolean {
+        let result = false;
+        for (let i = 0; i < 3; i++) {
+            if (this.board[0][i] === this.board[1][i] && this.board[0][i] === this.board[2][i]) {
+                this.solutions.push([[0, i], [1, i], [2, i]]);
+                result = true;
+            }
+            if (this.board[i][0] === this.board[i][1] && this.board[i][0] === this.board[i][2]) {
+                this.solutions.push([[i, 0], [i, 1], [i, 2]]);
+                result = true;
+            }
+        }
+        if (this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2]) {
+            this.solutions.push([[0, 0], [1, 1], [2, 2]]);
+            result = true;
+        }
+        if (this.board[0][2] === this.board[1][1] && this.board[0][2] === this.board[2][0]) {
+            this.solutions.push([[0, 2], [1, 1], [2, 0]]);
+            result = true;
+        }
+        return result;
+    }
+
+    public duplicate(): TicTacToe {
+        const result = new TicTacToe();
+        for (const [x, y] of this.moves) result.place(x, y);
+        return result;
+    }
+
+    public toString(payload?: TicTacToeToStringPayload): string {
+        if (!payload) payload = {
+            sign: ["❌", "⭕"],
+            winSign: ["❎", "🅾️"],
+            numbers: ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"],
+            separator: ""
+        };
+        const display = this.board.map(x =>
+            x.map(y => {
+                const index = parseInt(y, 10);
+                if (!isNaN(index)) return payload!.numbers[index - 1];
+                return payload!.sign[["X", "O"].indexOf(y)];
+            })
+        );
+        if (this.winner) {
+            for (const solution of this.solutions)
+                for (const [x, y] of solution)
+                    display[x][y] = payload.winSign[ ["X", "O"].indexOf(this.winner)];
+        }
+        return display.map(x => x.join(payload!.separator)).join("\n");
+    }
+
+    public giveUp(): void {
+        this.winner = this.turn ? "O" : "X";
+        this.end = true;
+    }
+
+    public placeAI(depth = 10): void {
+        const moves: { position: [number, number]; rate: number }[] = [];
+        for (let i = 1; i < 10; i++) {
+            const position = this.parsePosition(i);
+            if (!this.canPlace(...position)) continue;
+            const rate = this.doComplicatedThing(this.duplicate(), this.turn ? "X" : "O", position, depth);
+            moves.push({ position, rate });
+        }
+        const { rate: bestRate } = moves.sort((a, b) => b.rate - a.rate)[0];
+        const bestMoves = moves.filter(x => x.rate === bestRate);
+        this.place(...bestMoves[Math.floor(Math.random() * bestMoves.length)].position);
+    }
+
+    // Sometime just make stupid rate
+    private doComplicatedThing(ttt: TicTacToe, turn: string, position: [number, number], depth: number, curDepth = 0): number {
+        if (curDepth > depth) return 0;
+        ttt.place(...position);
+        if (ttt.end) return ttt.winner ? (ttt.winner === turn ? 1 : -1) : 0;
+        let result = 0;
+        for (let i = 1; i < 10; i++) {
+            const post = this.parsePosition(i);
+            if (!ttt.canPlace(...post)) continue;
+            const duplicated = ttt.duplicate();
+            const res = this.doComplicatedThing(duplicated, turn, post, depth, curDepth + 1);
+            result =+ res;
+        }
+        return result;
+    }
+}
--- src/commands/Fun/Games/TicTacToe.ts
+++ src/commands/Fun/Games/TicTacToe.ts
@@ -1,8 +1,10 @@
-import type YumekoClient from "../../../classes/Client";
 import Command from "../../../classes/Command";
-import type { Message, User, MessageReaction } from "discord.js";
+import TicTacToe from "../../../classes/Games/TicTacToe";
+import { Message, User, MessageReaction, Util } from "discord.js";
+import { DeclareCommand } from "../../../decorators";
 import { stripIndents } from "common-tags";
-import { chunk, verify } from "../../../util/Util";
+import { verify } from "../../../util/Util";
+import CustomError from "../../../classes/CustomError";
 
 const numbers = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"];
 
@@ -11,103 +13,67 @@ interface UpdateBoardReturn {
     board: string[][];
 }
 
+@DeclareCommand("game-tictactoe", {
+    aliases: [],
+    description: {
+        content: "Play Tic Tac Toe game with other user. This is solved game, you must placing 3 marks horizontal vertical or diagonal to win the match.",
+        usage: "<user>",
+        examples: ["game-tictactoe"],
+        adionalInfo: ["<:tictactoe:736370109073063946> Tic Tac Toe", "tictactoe", "ttt"]
+    },
+    category: "fun",
+    args: [
+        {
+            identifier: "opponent",
+            match: "rest",
+            type: "user:human",
+            optional: true
+        }
+    ]
+})
 export default class TicTacToeCommand extends Command {
-    public constructor (client: YumekoClient) {
-        super(client, "game-tictactoe", {
-            aliases: [],
-            description: {
-                content: "Play Tic Tac Toe game with other user. This is solved game, you must placing 3 marks horizontal vertical or diagonal to win the match.",
-                usage: "<user>",
-                examples: ["game-tictactoe"],
-                adionalInfo: ["<:tictactoe:736370109073063946> Tic Tac Toe", "tictactoe", "ttt"]
-            },
-            category: "fun",
-            args: [
-                {
-                    identifier: "opponent",
-                    match: "rest",
-                    type: "user:human",
-                    prompt: "Which user do you want to challenge ?"
+    public async exec(msg: Message, { opponent }: { opponent?: User }): Promise<Message> {
+        if (opponent) {
+            const verifyMsg = await msg.channel.send(`❓ **| ${opponent}, Do you want accept this challenge ?**`);
+            const verified = await verify(verifyMsg, opponent);
+            if (!verified) {
+                await verifyMsg.edit(`🍃 **| Look like ${opponent} doesn't accept your challenge. Do you want to play it with me anyway ?**`);
+                const accept = await verify(verifyMsg, msg.author);
+                if (!accept) {
+                    msg.ctx.send("✋ **| Ok see you next time**");
+                    throw new CustomError("CANCELED");
                 }
-            ]
-        });
-    }
-
-    public async exec(msg: Message, { opponent }: { opponent: User }): Promise<Message> {
-        const verifyMsg = await msg.channel.send(`❓ **| ${opponent}, Do you want accept this challenge ?**`);
-        const verified = await verify(verifyMsg, opponent);
-        if (!verified) return msg.ctx.send(`🍃 **| Look like ${opponent} doesn't accept your challenge**`);
-        const selected: string[] = [];
-        let board = chunk(numbers, 3);
-        let turn = true;
-        let passes = 0;
-        let winner: User|void;
+                opponent = this.client.user!;
+            }
+        } else opponent = this.client.user!;
         const message = await msg.channel.send("🖌️ **| Preparing**");
         for (const num of numbers) await message.react(num);
-        while (!winner && passes < 9) {
-            const user = turn ? msg.author : opponent;
+        const ttt = new TicTacToe();
+        while (!ttt.isEnd()) {
+            const user = ttt.turn ? msg.author : opponent;
             await message.edit(stripIndents`
                 <:tictactoe:736370109073063946> **| ${user}, Turn!**
-                > ${board.map(x => x.join("")).join("\n> ")}
+                > ${ttt.toString().replace(/\n/g, "\n> ")}
             `);
+            if (user.bot) {
+                await Util.delayFor(5000);
+                ttt.placeAI();
+                continue;
+            }
             const filter = (m: MessageReaction, usr: User): boolean => numbers.includes(m.emoji.name)
                 &&  usr.id === user.id
-                && !selected.includes(m.emoji.name);
+                && ttt.canPlace(...ttt.parsePosition(numbers.indexOf(m.emoji.name) + 1));
             const responses = await message.awaitReactions(filter, { max: 1, time: 30000 });
             if (!responses.size) {
-                winner = turn ? opponent : msg.author;
+                ttt.giveUp();
                 break;
             }
             const index = numbers.indexOf(responses.first()!.emoji.name);
-            const [line, column] = this.getIndexs(index);
-            selected.push(numbers[index]);
-            board[line][column] = turn ? "❌" : "⭕";
-            const check = this.updateBoard(board, turn ? "❎": "🅾️");
-            board = check.board;
-            if (check.isWin) winner = user;
-            turn = !turn;
-            passes++;
+            ttt.place(...ttt.parsePosition(index + 1));
         }
         return message.edit(stripIndents`
-            ${winner ? `🎉 **| Congrats ${winner}, you win the match!**` : "🇴 **| Draw!**"}
-            > ${board.map(x => x.join("")).join("\n> ")}
+            ${ttt.winner ? `🎉 **| Congrats ${ttt.turn ? msg.author : opponent}, you win the match!**` : "🇴 **| Draw!**"}
+            > ${ttt.toString().replace(/\n/g, "\n> ")}
         `);
-    }
-
-    public getIndexs(num: number): [number, number] {
-        return [
-            Math.floor(num/3),
-            num%3
-        ];
-    }
-    public updateBoard (board: string[][], checkSign: string): UpdateBoardReturn {
-        let result: UpdateBoardReturn = { isWin: false, board };
-        for (let i = 0; i < 3; i++) {
-            if (board[i][0] === board[i][1] && board[i][0] === board[i][2]){
-                board[i][0] = checkSign;
-                board[i][1] = checkSign;
-                board[i][2] = checkSign;
-                result = { board, isWin: true };
-            }
-            if (board[0][i] === board[1][i] && board[0][i] === board[2][i]){
-                board[0][i] = checkSign;
-                board[1][i] = checkSign;
-                board[2][i] = checkSign;
-                result = { board, isWin: true };
-            }
-        }
-        if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
-            board[0][0] = checkSign;
-            board[1][1] = checkSign;
-            board[2][2] = checkSign;
-            result = { board, isWin: true };
-        }
-        if (board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
-            board[0][2] = checkSign;
-            board[1][1] = checkSign;
-            board[2][0] = checkSign;
-            result = { board, isWin: true };
-        }
-        return result;
     }
 }
