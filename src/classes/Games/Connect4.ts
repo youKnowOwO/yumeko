0f8353e6b222917faa2f3f31feccdb0cc91ebdb5
OwO <41247578+youKnowOwO@users.noreply.github.com>
2020-08-20 01:11:18 +0000

    feat(game): Connect 4 🎮 (#46)
    * feat(game): Connect 4 🎮
    
    * feat(game-c4): Only refer to type
    
    * chore(game-ttt): AI is more faster++ [BONUS]

---
+++ src/classes/Games/Connect4.ts
@@ -0,0 +1,138 @@
+import { chunk } from "../../util/Util";
+
+interface Connect4ToStringPayload {
+    sign: [string, string];
+    winSign: [string, string];
+    emptySign: string;
+    separator: string;
+}
+
+export default class Connect4 {
+    public board = chunk(new Array<string>(42).fill(""), 7);
+    public end = false;
+    public winner?: string;
+    public solutions: [number, number][][] = [];
+    public turn = true; // true: player 1, false: player 2
+    public moves: number[] = [];
+
+    public isEnd(): boolean {
+        return this.end;
+    }
+
+    public place(col: number): void {
+        for (let i = 5; i >= 0; i--) {
+            if (!this.board[i][col].length) {
+                this.board[i][col] = this.turn ? "O" : "0";
+                this.moves.push(col);
+                break;
+            }
+        }
+        if (this.isWin()) {
+            this.end = true;
+            this.winner = this.turn ? "O" : "0";
+        }
+        if (this.moves.length > 41) this.end = true;
+        if (!this.isEnd()) this.turn = !this.turn;
+    }
+
+    public canPlace(col: number): boolean {
+        for (let i = 5; i >= 0; i--) {
+            if (!this.board[i][col].length) return true;
+        }
+        return false;
+    }
+
+    public isWin(): boolean {
+        const sign = this.turn ? "O" : "0";
+        let result = false;
+        for (let i = 0; i < 6; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (this.board[i][j] === sign && this.board[i][j + 1] === sign && this.board[i][j + 2] === sign && this.board[i][j + 3] === sign) {
+                    this.solutions.push([[i, j], [i, j + 1], [i, j + 2], [i, j + 3]]);
+                    result = true;
+                }
+            }
+        }
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 7; j++) {
+                if (this.board[i][j] === sign && this.board[i + 1][j] === sign && this.board[i + 2][j] === sign && this.board[i + 3][j] === sign){
+                    this.solutions.push([[i, j], [i + 1, j], [i + 2, j], [i + 3, j]]);
+                    result = true;
+                }
+            }
+        }
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (this.board[i][j] === sign && this.board[i + 1][j + 1] === sign && this.board[i + 2][j + 2] === sign && this.board[i + 3][j + 3] === sign) {
+                    this.solutions.push([[i, j], [i + 1, j + 1], [i + 2, j + 2], [i + 3, j + 3]]);
+                    result = true;
+                }
+            }
+        }
+        for (let i = 0; i < 3; i++) {
+            for (let j = 3; j < 7; j++) {
+                if (this.board[i][j] === sign && this.board[i + 1][j - 1] === sign && this.board[i + 2][j - 2] === sign && this.board[i + 3][j - 3] === sign) {
+                    this.solutions.push([[i, j], [i + 1, j - 1], [i + 2, j - 2], [i + 3, j - 3]]);
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    public duplicate(): Connect4 {
+        const result = new Connect4();
+        for (const move of this.moves) result.place(move);
+        return result;
+    }
+
+    public toString(payload?: Connect4ToStringPayload): string {
+        if (!payload) payload = {
+            sign: ["🔴", "🔵"],
+            winSign: ["🅾️", "🇴"],
+            emptySign: "⚪",
+            separator: ""
+        };
+        const display = this.board.map(x => x.map(y => payload!.sign[["O", "0"].indexOf(y)] || payload!.emptySign));
+        if (this.winner) {
+            for (const solution of this.solutions)
+                for (const [x, y] of solution)
+                    display[x][y] = payload.winSign[["O", "0"].indexOf(this.winner)];
+        }
+        return display.map(x => x.join(payload!.separator)).join("\n");
+    }
+
+    public giveUp(): void {
+        this.winner = this.turn ? "0" : "O";
+        this.end = true;
+    }
+
+    public placeAI(depth = 5): void {
+        const moves: { col: number; rate: number }[] = [];
+        for (let i = 0; i < 7; i++) {
+            if (!this.canPlace(i)) continue;
+            const duplicated = this.duplicate();
+            const rate = this.doComplicatedThing(duplicated, i, this.turn ? "O" : "0", depth);
+            moves.push({ col: i, rate });
+        }
+        const bestRate = Math.max(...moves.map(x => x.rate));
+        const bestMoves = moves.filter(x => x.rate === bestRate);
+        this.place(bestMoves[Math.floor(Math.random() * bestMoves.length)].col);
+    }
+
+    // Sometime just make stupid rate
+    private doComplicatedThing(c4: Connect4, col: number, turn: string, depth: number, curentDepth = 0, maximize = true): number {
+        if (curentDepth > depth) return 0;
+        c4.place(col);
+        const compliment = 42 - c4.moves.length;
+        if (c4.end) return c4.winner ? (c4.winner === turn ? compliment : -compliment) : 0;
+        let result = maximize ? -Infinity : Infinity;
+        for (let i = 0; i < 7; i++) {
+            if (!c4.canPlace(i)) continue;
+            const duplicated = c4.duplicate();
+            const res = c4.doComplicatedThing(duplicated, i, turn, depth, curentDepth + 1, !maximize);
+            result = Math[maximize ? "max" : "min"](result, res);
+        }
+        return result;
+    }
+}
--- src/commands/Fun/Games/Connect4.ts
+++ src/commands/Fun/Games/Connect4.ts
@@ -1,25 +1,20 @@
 import Command from "../../../classes/Command";
-import TicTacToe from "../../../classes/Games/TicTacToe";
 import CustomError from "../../../classes/CustomError";
-import type { Message, User, MessageReaction  } from "discord.js";
+import Connect4 from "../../../classes/Games/Connect4";
+import type { Message, MessageReaction, User } from "discord.js";
 import { DeclareCommand } from "../../../decorators";
-import { stripIndents } from "common-tags";
 import { verify } from "../../../util/Util";
+import { stripIndents } from "common-tags";
 
-const numbers = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"];
+const numbers = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣"];
 
-interface UpdateBoardReturn {
-    isWin: boolean;
-    board: string[][];
-}
-
-@DeclareCommand("game-tictactoe", {
+@DeclareCommand("game-connect4", {
     aliases: [],
     description: {
-        content: "Play Tic Tac Toe game with other user. This is solved game, you must placing 3 marks horizontal vertical or diagonal to win the match.",
+        content: "Play Connect4 game with other user. This is solved game, you must drop 4 marks horizontal vertical or diagonal to win the match.",
         usage: "<user>",
-        examples: ["game-tictactoe"],
-        adionalInfo: ["<:tictactoe:736370109073063946> Tic Tac Toe", "tictactoe", "ttt"]
+        examples: ["game-conmect4"],
+        adionalInfo: ["<:connect4:745791911218118706> Connect4", "connect4", "c4"]
     },
     category: "fun",
     args: [
@@ -31,7 +26,7 @@ interface UpdateBoardReturn {
         }
     ]
 })
-export default class TicTacToeCommand extends Command {
+export default class Connect4Command extends Command {
     public async exec(msg: Message, { opponent }: { opponent?: User }): Promise<Message> {
         if (opponent) {
             const verifyMsg = await msg.channel.send(`❓ **| ${opponent}, Do you want accept this challenge ?**`);
@@ -48,31 +43,33 @@ export default class TicTacToeCommand extends Command {
         } else opponent = this.client.user!;
         const message = await msg.channel.send("🖌️ **| Preparing**");
         for (const num of numbers) await message.react(num);
-        const ttt = new TicTacToe();
-        while (!ttt.isEnd()) {
-            const user = ttt.turn ? msg.author : opponent;
+        const c4 = new Connect4();
+        while(!c4.isEnd()) {
+            const user = c4.turn ? msg.author : opponent;
             await message.edit(stripIndents`
-                <:tictactoe:736370109073063946> **| ${user}, Turn!**
-                > ${ttt.toString().replace(/\n/g, "\n> ")}
+                <:connect4:745791911218118706> **| ${user}, Turn!**
+                > ${c4.toString().replace(/\n/g, "\n> ")}
+                > ${numbers.join("")}
             `);
             if (user.bot) {
-                ttt.placeAI();
+                c4.placeAI();
                 continue;
             }
-            const filter = (m: MessageReaction, usr: User): boolean => numbers.includes(m.emoji.name)
-                &&  usr.id === user.id
-                && ttt.canPlace(...ttt.parsePosition(numbers.indexOf(m.emoji.name) + 1));
+            const filter = (m: MessageReaction, usr: User): boolean => usr.id === user.id
+                && numbers.includes(m.emoji.name)
+                && c4.canPlace(numbers.indexOf(m.emoji.name));
             const responses = await message.awaitReactions(filter, { max: 1, time: 30000 });
             if (!responses.size) {
-                ttt.giveUp();
+                c4.giveUp();
                 break;
             }
             const index = numbers.indexOf(responses.first()!.emoji.name);
-            ttt.place(...ttt.parsePosition(index + 1));
+            c4.place(index);
         }
         return message.edit(stripIndents`
-            ${ttt.winner ? `🎉 **| Congrats ${ttt.turn ? msg.author : opponent}, you win the match!**` : "🇴 **| Draw!**"}
-            > ${ttt.toString().replace(/\n/g, "\n> ")}
+            ${c4.winner ? `🎉 **| Congrats ${c4.turn ? msg.author : opponent}, you win the match!**` : "🇴 **| Draw!**"}
+            > ${c4.toString().replace(/\n/g, "\n> ")}
+            > ${numbers.join("")}
         `);
     }
 }
