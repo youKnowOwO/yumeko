1b3c9d6a37278729568989e27567818052722ec0
youKnowOwO <meiterumidikolam@gmail.com>
2020-08-26 23:26:51 +0800

    feat: Lyrics 📄

---
+++ src/commands/Music/Lyrics.ts
@@ -0,0 +1,60 @@
+import Command from "@yumeko/classes/Command";
+import request from "node-superfetch";
+import type { Message } from "discord.js";
+import { DeclareCommand } from "@yumeko/decorators";
+import { chunk } from "@yumeko/util/Util";
+import { MessageEmbed } from "discord.js";
+import Pagination from "@yumeko/util/Pagination";
+
+interface LyricsResponse {
+    title: string;
+    author: string;
+    lyrics: string;
+    thumbnail: {
+        genius: string;
+    };
+    links: {
+        genius: string;
+    };
+}
+
+@DeclareCommand("lyrics", {
+    aliases: ["lyrics", "lyrics"],
+    description: {
+        content: "Get lyrics from query based",
+        usage: "lyrics",
+        examples: ["lyrics Moo Doja Cat"]
+    },
+    category: "music",
+    args: [
+        {
+            identifier: "title",
+            match: "rest",
+            type: "string",
+            prompt: "What the title of the song ?"
+        }
+    ]
+})
+export default class LyricsCommand extends Command {
+    public async exec(msg: Message, { title }: { title: string }): Promise<Message> {
+        const result = await this.getLyrics(title);
+        if (!result) return msg.ctx.send("🚫 No result found");
+        const pages = chunk(result.lyrics, 2048);
+        const embed = new MessageEmbed()
+            .setColor(this.client.config.color)
+            .setURL(result.links.genius)
+            .setTitle(result.title)
+            .setThumbnail(result.thumbnail.genius);
+        return new Pagination(msg, {
+            embed, pages,
+            edit: (index, emb, page): MessageEmbed => emb.setDescription(page).setFooter(`Page ${index + 1} of ${pages.length}`)
+        }).start();
+    }
+
+    public async getLyrics(title: string): Promise<LyricsResponse|void> {
+        const { body }: any = await request.get("https://some-random-api.ml/lyrics")
+            .query({ title });
+        if (body.error) return undefined;
+        return body;
+    }
+}
--- src/util/Util.ts
+++ src/util/Util.ts
@@ -16,10 +16,13 @@ export function firstUpperCase(text: string): string {
     return `${text.charAt(0).toUpperCase()}${text.slice(1)}`;
 }
 
-export function chunk<T>(arr: T[], len: number): T[][] {
-    const rest: T[][] = [];
+export function chunk<T>(arr: T[], len: number): T[][];
+export function chunk(arr: string, len: number): string[];
+export function chunk(...args: any[]): any[] {
+    const [arr, len] = args as [any, number];
+    const rest: (typeof arr)[] = [];
     for (let i = 0; i < arr.length; i += len)
-        rest.push(arr.slice(i, i+len));
+        rest.push(arr.slice(i, i + len));
     return rest;
 }
 
