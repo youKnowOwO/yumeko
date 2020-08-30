f5ad3dd03e3f91e8d9474170b6d28a37f96bebb5
youKnowOwO <meiterumidikolam@gmail.com>
2020-08-30 08:16:18 +0800

    chore: Renew schema

--- src/langs/en_US.ts
+++ src/langs/en_US.ts
@@ -2,9 +2,9 @@ import type { User } from "discord.js";
 
 
 export default {
-    META_NAME: "English (US)",
-    META_ID: "en_US",
-    COMMAND_RUNNER_ONLY_NSFW: "❌ **| This command only work on nsfw channel**",
+    META_NAME: (): string => "English (US)",
+    META_ID: (): string => "en_US",
+    COMMAND_RUNNER_ONLY_NSFW: (): string => "❌ **| This command only work on nsfw channel**",
     COMMAND_RUNNER_ONLY_ONE: (user: User): string => `**❌ | ${user}, Only one command per user**`,
     COMMAND_RUNNER_IN_COOLDOWN: (user: User, amount: number): string => `**⏱️ | ${user}, Calm down!.** You can use command again in \`${Math.round(amount)}\` second(s)`,
     COMMAND_RUNNER_MISSPERMS: (user: User, perm: string, isclient = false): string => `**❌ | ${isclient ? "Im" : `${user}, You`} require this permission(s) to run the command. ${perm}**`
--- src/langs/id_ID.ts
+++ src/langs/id_ID.ts
@@ -1,5 +1,11 @@
-import en_US from "./en_US";
+import en_US from "@yumeko/langs/en_US";
 
 export default {
-    ...en_US
+    ...en_US,
+    META_NAME: () => "Indonesia",
+    META_ID: () => "id_ID",
+    COMMAND_RUNNER_ONLY_NSFW: () => "❌ **| Command ini hanya akan work dalam channel NSFW**",
+    COMMAND_RUNNER_ONLY_ONE: user => `**❌ | ${user}, kamu tidak bisa menggunakan command lebih dari 1**`,
+    COMMAND_RUNNER_IN_COOLDOWN: (user, amount) => `**⏱️ | ${user}, sabar!.** kamu bisa menggunakan command lagi dalam \`${Math.round(amount)}\` detik`,
+    COMMAND_RUNNER_MISSPERMS: (user, perm, isclient) => `**❌ | ${isclient ? "Aku" : `${user}, kamu`} membutuhkan permission ini untuk menjalankan command. ${perm}**`
 } as typeof en_US;
